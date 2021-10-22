b7ae42927b6bd75d1121d690bf4f45f140b0f622
LittleDevilBig <47845786+LittleDevilBig@users.noreply.github.com>
2021-10-22 23:18:42 +0800

    维护odd和even odd=max（odd，even+n） even=odd-n

---
+++ 奇加偶减.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    long long n,odd=0,even=0;
+    while(cin>>n){
+        odd=max(odd,even+n);
+        even=odd-n;
+        cout<<odd;
+    }
+    cout<<odd;
+}
